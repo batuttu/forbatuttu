742d2368fbefcda849e0d4bc7ee899a7b44f3b48
hamsi <pmrt276@gmail.com>
2023-12-05 19:14:19 +0300

    Up

---
+++ app.js
@@ -0,0 +1,139 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const fs = require('fs');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = 3030;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
+// Dosya yolu
+const jsonFilePath = 'public/dates.json';
+
+function update(data){
+    console.log("BURASI "+data.username);
+    console.log("BURASI "+data.dates);
+    fs.readFile(jsonFilePath, 'utf8', (err, jsondata) => {
+        if (err) {
+            console.error('Dosya okuma hatası:', err);
+            return;
+        }
+    
+        // JSON verisini parse et
+        const jsonData = JSON.parse(jsondata);
+    
+        // Kontrol ve Güncelleme Fonksiyonu
+        function updateJsonData(data) {
+            // Kullanıcı var mı kontrol et
+            const userIndex = jsonData.findIndex(user => user.username === data.username);
+    
+            // Eğer kullanıcı yoksa ekle
+            if (userIndex === -1) {
+                jsonData.push(data);
+                console.log(data.username + " kullanıcısı eklenmiştir.");
+            } else {
+                
+                // Tarih dizisi içinde THE_Tarh var mı kontrol et
+                console.log(jsonData[userIndex].dates);
+                var index = jsonData[userIndex].dates.indexOf(data.dates);
+            
+                if (index !== -1) {
+                    // Eğer varsa, tarihi sil
+                    jsonData[userIndex].dates.splice(index, 1);
+                    console.log("silindi");
+                } else {
+                    // Eğer yoksa, tarihi ekle
+                    console.log("ABs " +data.dates);
+                    jsonData[userIndex].dates.push(data.dates);
+                    console.log("eklendi");
+                }
+            }
+    
+            // Güncellenmiş JSON'u dosyaya yaz
+            fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
+                if (err) {
+                    console.error('Dosya yazma hatası:', err);
+                    return;
+                }
+                console.log('JSON dosyası güncellenmiştir.');
+            });
+        }
+        updateJsonData(data);
+    });
+}
+
+function addNewUser(data){
+    fs.readFile(jsonFilePath, 'utf8', (err, jsondata) => {
+        if (err) {
+            console.error('Dosya okuma hatası:', err);
+            return;
+        }
+    
+        // JSON verisini parse et
+        const jsonData = JSON.parse(jsondata);
+    
+        // Kontrol ve Güncelleme Fonksiyonu
+        function addNewUserJsonData(data) {
+            // Kullanıcı var mı kontrol et
+            const userIndex = jsonData.findIndex(user => user.username === data.username);
+    
+            // Eğer kullanıcı yoksa ekle
+            if (userIndex === -1) {
+                jsonData.push(data);
+                io.emit('isAvailable', "yes");
+            }else{
+                // YOU CANT GET THIS USERNAME
+                io.emit('isAvailable', "no");
+            }
+    
+            // Güncellenmiş JSON'u dosyaya yaz
+            fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
+                if (err) {
+                    console.error('Dosya yazma hatası:', err);
+                    return;
+                }
+                console.log('JSON dosyası güncellenmiştir.');
+            });
+        }
+        addNewUserJsonData(data);
+    });
+}
+
+io.on('connection', (socket) => {
+    console.log('Bir kullanıcı bağlandı');
+    
+    // Yeni bir mesaj alındığında
+    socket.on('istiyorum', (data) => {
+        console.log(data);
+        update(data);
+        console.log(data.username +' Tarafından, İstenilen Tarih: ' + data.dates);
+    });
+    // Yeni bir mesaj alındığında
+    socket.on('sil', (data) => {
+        console.log(data);
+        update(data);
+        console.log(data.username +' Tarafından, Silinen Tarih: ' + data.dates);
+    });
+    socket.on('newUser', (data) => {
+        console.log(data);
+        addNewUser(data);
+    });
+    // Bağlantı kesildiğinde
+    socket.on('disconnect', () => {
+        console.log('Bir kullanıcı ayrıldı');
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,130 @@
+var socket = io();
+const login_button = document.querySelector(".login-buton");
+const user_name_input = document.querySelector(".the-name");
+const login_screen = document.querySelector(".login-screen");
+const takvim_screen = document.querySelector(".takvim-screen");
+const alert_div = document.querySelector(".alert-div");
+var iAm;
+// LOCALE STROGE
+if(localStorage.getItem('VEGA_Takvim') == null){
+    login_screen.style.display = "flex";
+    takvim_screen.style.display = "none";
+}else{
+    login_screen.style.display = "none";
+    takvim_screen.style.display = "flex";
+}
+
+function addMyName(){
+    console.log(user_name_input.value);
+    var data = {
+        "username" :  user_name_input.value,
+        "dates" : []
+    }
+
+    console.log(data);
+
+    socket.emit('newUser', data);
+    socket.on('isAvailable', (data) => {
+        if(data == "yes"){
+            localStorage.setItem('VEGA_Takvim', user_name_input.value);
+            iAm = localStorage.getItem('VEGA_Takvim');
+            if(localStorage.getItem('VEGA_Takvim') == null){
+                login_screen.style.display = "flex";
+                takvim_screen.style.display = "none";
+            }else{
+                login_screen.style.display = "none";
+                takvim_screen.style.display = "flex";
+            }
+        }else if(data == "no"){
+            alert_div.innerText = "Bu Adı Alamazsın";
+            alert_div.style.display = "flex";
+        }
+    });
+}
+
+login_button.addEventListener("click",()=>{
+    addMyName();
+});
+
+const calendar_box = document.querySelector(".calendar-container");
+
+
+
+var startDate = new Date("2023-12-05");
+var endDate = new Date("2023-12-30");
+
+// Her gün için döngü
+while (startDate <= endDate) {
+    // Her gün için bir kutu oluştur
+    var dayBox = document.createElement('div');
+    dayBox.classList.add('day-box');
+    dayBox.setAttribute('isCheck', "false");
+    dayBox.textContent = startDate.toLocaleDateString();
+
+    // Kutuyu ana container'a ekle
+    calendar_box.appendChild(dayBox);
+    
+
+    dayBox.addEventListener("click", (e) => {
+        var thisDate = e.target.innerText;
+        console.log(thisDate);
+        console.log(e.target.getAttribute("isCheck"));
+        e.target.style.backgroundColor = "green";
+
+        if(e.target.getAttribute("isCheck") == "false"){
+            e.target.setAttribute('isCheck', "true");
+            e.target.style.backgroundColor = "green";
+            console.log("istiyorum iam " +iAm);
+            var data = 
+                {
+                    "username" : iAm,
+                    "dates" : thisDate
+                }
+            console.log(data);
+            socket.emit('istiyorum', data);
+        }
+        else{
+            e.target.setAttribute('isCheck', "false");
+            e.target.style.backgroundColor = "red";
+            var data = 
+                {
+                    "username" : iAm,
+                    "dates" : thisDate
+                }
+            socket.emit('sil', data);
+        }
+        console.log(e.target.getAttribute("isCheck"));
+    })
+    
+    startDate.setDate(startDate.getDate() + 1);
+}
+
+if(localStorage.getItem('VEGA_Takvim')){
+    iAm = localStorage.getItem('VEGA_Takvim');
+    fetch('dates.json')
+    .then(response => response.json())
+    .then(jsondata => {
+
+        console.log(jsondata);
+        var boxes = document.querySelectorAll(".day-box");
+
+        const userIndex = jsondata.findIndex(user => user.username === iAm);
+        console.log(userIndex);
+        if(jsondata[userIndex].dates.length > 0){
+            jsondata[userIndex].dates.forEach(date => {
+                boxes.forEach(box => {
+                    if(date == box.innerText){
+                        box.setAttribute('isCheck', "true");
+                        box.style.backgroundColor = "green";
+                    }
+                })
+            });
+        }
+    });
+}
+
+user_name_input.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+        addMyName();
+    }
+})
